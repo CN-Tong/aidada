c4a4e1eb624c64bfb0006e6c547d3c2089b3fee4
CN-Tong <ZHT2018060708@163.com>
2024-06-04 14:34:38 +0800

    爱答答 项目后端初始化

---
+++ src/main/java/com/tong/aidada/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.tong.aidada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tong.aidada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
